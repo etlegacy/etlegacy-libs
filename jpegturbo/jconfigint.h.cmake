b09ec8db884a30c7b522f1278e9d647d9454d55b
Remy Marquis <remy.marquis@gmail.com>
2017-07-01 18:56:13 +0200

    libs: added libjpeg-turbo cmake files

---
+++ jpegturbo/jconfigint.h.cmake
@@ -0,0 +1,14 @@
+/* libjpeg-turbo build number */
+#undef BUILD
+
+/* How to obtain function inlining. */
+#define INLINE __inline
+
+/* Define to the full name of this package. */
+#undef PACKAGE_NAME
+
+/* Version number of package */
+#undef VERSION
+
+/* The size of `size_t', as computed by sizeof. */
+#undef SIZEOF_SIZE_T
